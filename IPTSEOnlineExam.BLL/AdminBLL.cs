19dd275ac37f6268c8ca859a44be1deca76f1003
Maneesh <30887512+maneesh-kumar@users.noreply.github.com>
2018-11-01 14:58:53 +0530

    Added admin section

---
+++ IPTSEOnlineExam.BLL/AdminBLL.cs
@@ -0,0 +1,202 @@
+﻿using IPTSEOnlineExam.BLL.Models;
+using IPTSEOnlineExam.Common;
+using IPTSEOnlineExam.DAL;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Validation;
+using System.Data.Objects;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPTSEOnlineExam.BLL
+{
+    public class AdminBLL : IDisposable
+    {
+        public List<Test> GetExam()
+        {
+            IPTSE_EXAMEntities objEntities = new IPTSE_EXAMEntities();
+            return objEntities.tbl_Test.Where(n => (bool)n.IsActive).
+                Select(m => new Test { TestId = m.TestId, Name = m.Name, Description = m.Description, DurationInMinutes = m.DurationInMinutes }).ToList();
+        }
+
+        public List<Questions> GetQuestions()
+        {
+            IPTSE_EXAMEntities db = new IPTSE_EXAMEntities();
+            return db.tbl_Question.Select(m => new Questions
+            {
+                Id = m.Id,
+                IsActive = m.IsActive,
+                QuestionText = m.QuestionText,
+                QuestionCategoryId = m.QuestionCategoryId,
+                TestId = m.tbl_Test_Question_Map.FirstOrDefault().TestId
+            }).ToList();//.Include(m => m.questionsChoice).ToList();//.Include(q => q.Exam_tbl);
+
+        }
+
+        public Questions GetQuestionDetails(int? id)
+        {
+            using (IPTSE_EXAMEntities db = new IPTSE_EXAMEntities())
+            {
+                tbl_Question ques = db.tbl_Question.Find(id);
+                if (ques == null)
+                {
+                    return null;
+                }
+                else
+                {
+                    return new Questions
+                    {
+                        Id = ques.Id,
+                        QuestionText = ques.QuestionText,
+                        IsActive = ques.IsActive,
+                        QuestionCategoryId = ques.QuestionCategoryId,
+                        questionsChoice = ques.tbl_Question_Choice.Select(m => new QuestionsChoice
+                        {
+                            Id = m.Id,
+                            ChoiceText = m.ChoiceText,
+                            IsActive = m.IsActive,
+                            IsAnswer = m.IsAnswer,
+                            Question_Id = m.Question_Id
+                        }).ToList(),
+                        TestId = ques.tbl_Test_Question_Map.Where(m => m.QuestionId == ques.Id).FirstOrDefault().TestId
+                    };
+                }
+            }
+
+            //if (questions_tbl == null)
+            //{
+            //    return HttpNotFound();
+            //}
+        }
+
+        public List<QuestionCategory> GetQuestionCategory()
+        {
+            IPTSE_EXAMEntities objEntities = new IPTSE_EXAMEntities();
+            return objEntities.tbl_Question_Category.Select(m => new QuestionCategory
+            {
+                Category = m.Category,
+                Id = m.Id
+            }).ToList();
+        }
+
+        public void AddQuestion(Questions in_question)
+        {
+
+            using (IPTSE_EXAMEntities db = new IPTSE_EXAMEntities())
+            {
+                using (var dbcxtransaction = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        tbl_Question question = new tbl_Question();
+                        tbl_Question_Category question_Category = new tbl_Question_Category();
+                        tbl_Question_Choice question_Choice = new tbl_Question_Choice();
+                        //tbl_Test test = new tbl_Test();
+                        tbl_Test_Question_Map test_question_Map = new tbl_Test_Question_Map();
+
+                        question.QuestionText = in_question.QuestionText;
+                        question.IsActive = in_question.IsActive;
+                        question.Points = in_question.Points;
+                        question.QuestionCategoryId = (int)in_question.QuestionCategoryId;
+                        question.CreatedBy = "Admin";
+                        question.CreatedDate = DateTime.Now;
+                        db.tbl_Question.Add(question);
+                        db.SaveChanges();
+                        //question.Id
+
+                        db.tbl_Test_Question_Map.Add(new tbl_Test_Question_Map
+                        {
+                            TestId = in_question.TestId,
+                            QuestionId = question.Id,
+                            IsActive = in_question.IsActive,
+                            CreatedBy = "Admin",
+                            CreatedDate = DateTime.Now
+                        });
+
+                        foreach (QuestionsChoice item in in_question.questionsChoice)
+                        {
+                            db.tbl_Question_Choice.Add(new tbl_Question_Choice
+                            {
+                                ChoiceText = item.ChoiceText,
+                                IsActive = item.IsActive,
+                                IsAnswer = item.IsAnswer,
+                                Question_Id = question.Id,
+                                CreatedBy = "Admin",
+                                CreatedDate = DateTime.Now
+                                //,tbl_Question = question
+                            });
+                        }
+                        db.SaveChanges();
+                        dbcxtransaction.Commit();
+                    }
+                    catch (DbEntityValidationException ex)
+                    {
+                        dbcxtransaction.Rollback();
+                        throw ex;
+                    }
+                    catch (Exception ex)
+                    {
+                        dbcxtransaction.Rollback();
+                        throw ex;
+                    }
+
+                }
+            }
+        }
+
+        public void SaveChanges(Questions ques)
+        {
+            using (IPTSE_EXAMEntities db = new IPTSE_EXAMEntities())
+            {
+                using (var dbcxtransaction = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        tbl_Question tbl_Ques = new tbl_Question();
+                        tbl_Ques.Id = ques.Id;
+                        tbl_Ques.QuestionText = ques.QuestionText;
+                        tbl_Ques.QuestionCategoryId = (int)ques.QuestionCategoryId;
+                        tbl_Ques.IsActive = ques.IsActive;
+                        tbl_Ques.CreatedBy = "Admin";
+                        tbl_Ques.CreatedDate = DateTime.Now;
+                        ques.questionsChoice.ForEach(m => tbl_Ques.tbl_Question_Choice.Add(new tbl_Question_Choice
+                        {
+                            Id = m.Id,
+                            Question_Id = m.Question_Id,
+                            ChoiceText = m.ChoiceText,
+                            IsAnswer = m.IsAnswer,
+                            IsActive = m.IsActive,
+                            CreatedBy = "Admin",
+                            CreatedDate = tbl_Ques.CreatedDate
+                        }));
+
+                        db.Entry(tbl_Ques).State = EntityState.Modified;
+                        foreach (var item in tbl_Ques.tbl_Question_Choice)
+                        {
+                            db.Entry(item).State = EntityState.Modified;
+                        }
+                        db.SaveChanges();
+                        dbcxtransaction.Commit();
+                    }
+                    catch (DbEntityValidationException ex)
+                    {
+                        dbcxtransaction.Rollback();
+                        throw ex;
+                    }
+                    catch (Exception ex)
+                    {
+                        dbcxtransaction.Rollback();
+                        throw ex;
+                    }
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ IPTSEOnlineExam/Controllers/QuestionController.cs
@@ -0,0 +1,140 @@
+﻿using IPTSEOnlineExam.BLL;
+using IPTSEOnlineExam.BLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using System.Web.UI;
+
+namespace IPTSEOnlineExam.Controllers
+{
+    public class QuestionController : Controller
+    {
+        // GET: MockTest
+        public ActionResult Index()
+        {
+            AdminBLL adminBLL = new AdminBLL();
+            return View(adminBLL.GetQuestions());
+        }
+
+        // GET: MockTest/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            AdminBLL adminBLL = new AdminBLL();
+            Questions question = adminBLL.GetQuestionDetails(id);
+            if (question == null)
+            {
+                return HttpNotFound();
+            }
+            return View(question);
+        }
+
+        // GET: MockTest/Create
+        public ActionResult Create()
+        {
+            AdminBLL adminBLL = new AdminBLL();
+            ViewBag.exam_id = new SelectList(adminBLL.GetExam(), "TestId", "Name");
+            ViewBag.category_id = new SelectList(adminBLL.GetQuestionCategory(), "Id", "Category");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Questions questions_tbl)
+        {
+            AdminBLL adminBLL = new AdminBLL();
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    adminBLL.AddQuestion(questions_tbl);
+                    return RedirectToAction("Index");
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.ErrorMessage = ex.InnerException;
+                    throw ex;
+                }
+            }
+
+            ViewBag.exam_id = new SelectList(adminBLL.GetExam(), "TestId", "Name", questions_tbl.TestId);
+            ViewBag.category_id = new SelectList(adminBLL.GetQuestionCategory(), "Id", "Category", questions_tbl.QuestionCategoryId);
+            return View(questions_tbl);
+        }
+
+        // GET: MockTest/Edit/5
+        public ActionResult Edit(int id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AdminBLL adminBLL = new AdminBLL();
+            Questions question = adminBLL.GetQuestionDetails(id);
+            if (question == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.exam_id = new SelectList(adminBLL.GetExam(), "TestId", "Name", question.TestId);
+            ViewBag.category_id = new SelectList(adminBLL.GetQuestionCategory(), "Id", "Category", question.QuestionCategoryId);
+
+            return View(question);
+
+        }
+
+        // POST: MockTest/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Questions ques)
+        {
+            AdminBLL adminBLL = new AdminBLL();
+            try
+            {
+
+                if (ModelState.IsValid)
+                {
+                    adminBLL.SaveChanges(ques);
+                    return RedirectToAction("Index");
+                }
+                ViewBag.exam_id = new SelectList(adminBLL.GetExam(), "TestId", "Name", ques.TestId);
+                ViewBag.category_id = new SelectList(adminBLL.GetQuestionCategory(), "Id", "Category", ques.QuestionCategoryId);
+                return View(ques);
+            }
+            catch (Exception ex)
+            {
+                return View();
+            }
+
+        }
+
+        // GET: MockTest/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: MockTest/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
